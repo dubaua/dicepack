597b6cd2a3dba9b36c321b256cc002713a77150a
dubaua <dubaua@gmail.com>
2019-02-28 16:06:42 +0500

    convert unnecessary arrow function to declaration

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import {
 const DICE_EXPRESSION_REGEXP = /^((-?)(\d+|\d*d\d+))([+-](\d+|\d*d\d+))*$/;
 const SINGLE_DICE_REGEXP = /^-?\d*d\d+$/;
 
-const validate = expression => {
+function validate(expression) {
   if (!DICE_EXPRESSION_REGEXP.test(expression)) {
     throw new Error("Given dice expression isn't valid.");
   }
   return expression;
-};
+}
 
-const parseSingleExpression = expression => {
+function parseSingleExpression(expression) {
   if (!SINGLE_DICE_REGEXP.test(expression)) {
     return castToNumber(expression);
   }
@@ -27,36 +27,26 @@ const parseSingleExpression = expression => {
   const count = Math.abs(_count) || 1;
   const sign = isNegative ? -1 : 1;
   return { count, side, sign };
-};
+}
 
 const tryNumber = fn => config =>
   typeof config === "number" ? config : fn(config);
 
-const collectResultsFor = (fn, config, detailed) => {
+function collectResultsFor(fn, config, detailed) {
   const rolls = config.map(tryNumber(fn)).reduce(flatten, []);
   const result = rolls.reduce(sum, 0);
   if (detailed) {
     return { rolls, result };
   }
   return result;
-};
+}
 
-/**
- * Pasre D&D expression to dice configuration for further rolling.
- * @param {string} expression - D&D dice expression
- * @returns {Array.<object, number} array of dice configs  or numbers
- */
 export const parseExpression = expression =>
   validate(expression)
     .replace(/-/g, "+-")
     .split("+")
     .map(parseSingleExpression);
 
-/**
- *
- *
- * @param {*} expression
- */
 export const rollExpression = (expression, detailed) =>
   rollDiceConfig(parseExpression(expression), detailed);
 
