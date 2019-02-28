3c3b212e13f98c34bc936eb03adc4d7a03ce3e15
dubaua <dubaua@gmail.com>
2019-02-28 16:08:37 +0500

    order in library, remove polymorphism, rework random for performance

--- src/index.js
+++ src/index.js
@@ -1,60 +1,79 @@
-import {
-  rollDice,
-  minDice,
-  maxDice,
-  flatten,
-  sum,
-  castToNumber
-} from "./utils.js";
+import { flatten, sum, castToNumber, getRandomInt } from  "./utils.js";
 
 const DICE_EXPRESSION_REGEXP = /^((-?)(\d+|\d*d\d+))([+-](\d+|\d*d\d+))*$/;
-const SINGLE_DICE_REGEXP = /^-?\d*d\d+$/;
+const SINGLE_DICE_REGEXP = /^-?(\d+|\d*d\d+)$/;
 
-function validate(expression) {
-  if (!DICE_EXPRESSION_REGEXP.test(expression)) {
-    throw new Error("Given dice expression isn't valid.");
+const validate = function(expression) {
+  if (
+    typeof expression === "string" &&
+    !DICE_EXPRESSION_REGEXP.test(expression)
+  ) {
+    throw new Error("Given dice expression isn't valid");
   }
   return expression;
-}
+};
 
-function parseSingleExpression(expression) {
+const getDice = function(expression) {
   if (!SINGLE_DICE_REGEXP.test(expression)) {
-    return castToNumber(expression);
+    throw new Error("Given single dice expression isn't valid");
   }
-  const isNegative = expression.charAt(0) === "-";
-  const _expression = isNegative ? expression.slice(1) : expression;
-  const [_count, side] = _expression.split("d").map(castToNumber);
+  const [_count, _side] = expression.split("d").map(castToNumber);
   const count = Math.abs(_count) || 1;
-  const sign = isNegative ? -1 : 1;
+  const side = _side || 1;
+  const sign = _count < 0 ? -1 : 1;
   return { count, side, sign };
-}
+};
 
-const tryNumber = fn => config =>
-  typeof config === "number" ? config : fn(config);
-
-function collectResultsFor(fn, config, detailed) {
-  const rolls = config.map(tryNumber(fn)).reduce(flatten, []);
-  const result = rolls.reduce(sum, 0);
-  if (detailed) {
-    return { rolls, result };
-  }
-  return result;
-}
-
-export const parseExpression = expression =>
+const parse = expression =>
   validate(expression)
     .replace(/-/g, "+-")
     .split("+")
-    .map(parseSingleExpression);
+    .map(getDice);
 
-export const rollExpression = (expression, detailed) =>
-  rollDiceConfig(parseExpression(expression), detailed);
+const rollDie = function({ count, side, sign }) {
+  let results = [];
+  // no need to roll d1
+  if (side === 1) {
+    results.push(count * sign);
+  } else {
+    for (let i = 0; i < count; i++) {
+      results.push(getRandomInt(side) * sign);
+    }
+  }
+  return results;
+};
 
-export const rollDiceConfig = (config, detailed) =>
-  collectResultsFor(rollDice, config, detailed);
+const minimize = function({ count, side, sign }) {
+  // maximize in case of negative roll
+  const _side = sign === -1 ? side : 1;
+  return count * sign * _side;
+};
 
-export const minDiceConfig = (config, detailed) =>
-  collectResultsFor(minDice, config, detailed);
+const maximize = function({ count, side, sign }) {
+  // minimize in case of negative roll
+  const _side = sign === -1 ? 1 : side;
+  return count * sign * _side;
+};
 
-export const maxDiceConfig = (config, detailed) =>
-  collectResultsFor(maxDice, config, detailed);
+const collect = function(results, detailed) {
+  const rolls = results.reduce(flatten, []);
+  const result = rolls.reduce(sum, 0);
+  if (detailed) {
+    return { result, rolls };
+  }
+  return result;
+};
+
+const rollDice = (dice, detailed) => collect(dice.map(rollDie), detailed);
+
+const minDice = (dice, detailed) => collect(dice.map(minimize), detailed);
+
+const maxDice = (dice, detailed) => collect(dice.map(maximize), detailed);
+
+const roll = (expression, detailed) => rollDice(parse(expression), detailed);
+
+const min = (expression, detailed) => minDice(parse(expression), detailed);
+
+const max = (expression, detailed) => maxDice(parse(expression), detailed);
+
+export { parse, rollDice, minDice, maxDice, roll, min, max };
--- src/utils.js
+++ src/utils.js
@@ -7,30 +7,4 @@ export const flatten = (accumulator, current) =>
     ? accumulator.concat(current.reduce(flatten, []))
     : accumulator.concat(current);
 
-export const rollDice = ({ count, side, sign }) =>
-  times(() => rollDie(side), count).map(result => result * sign);
-
-export const minDice = ({ count, side, sign }) =>
-  sign === -1 ? count * side * sign : count * sign;
-
-export const maxDice = ({ count, side, sign }) =>
-  sign === -1 ? count * sign : count * side * sign;
-
-export const times = (fn, count) => {
-  var willReturn = [];
-  let step = 0;
-  while (step < count) {
-    willReturn[step] = fn();
-    step++;
-  }
-  return willReturn;
-};
-
-export const rollDie = side => Math.floor(random() * side) + 1;
-
-export const random = () => {
-  const _crypto = window.crypto || window.msCrypto;
-  let array = new Uint32Array(1);
-  _crypto.getRandomValues(array);
-  return array[0] * Math.pow(2, -32);
-};
+export const getRandomInt = max => Math.floor(Math.random() * max) + 1;
