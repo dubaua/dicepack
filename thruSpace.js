5a3f04648ed9d027f10593f3888b045b54880a52
dubaua <dubaua@gmail.com>
2019-04-04 15:15:30 +0500

    add star sky div drawing

--- thruSpace.js
+++ thruSpace.js
@@ -1,8 +1,5 @@
 import { getDiceSet } from './index.js';
-import { create, bindReference } from './render.js';
-
-const refs = {};
-const fpsDelay = fpsDelay;
+import { create } from './render.js';
 
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
@@ -10,58 +7,77 @@ const screenHeight = window.innerHeight;
 const screenCenterX = Math.floor(screenWidth / 2);
 const screenCenterY = Math.floor(screenHeight / 2);
 
-const xRange = getDiceSet(`4d${Math.round(screenWidth / 4) }`);
-const yRange = getDiceSet(`2d${Math.round(screenHeight / 2) }`);
+// const xRange = getDiceSet(`d${Math.round(screenWidth)}`);
+// const yRange = getDiceSet(`d${Math.round(screenHeight)}`);
+const xRange = getDiceSet(`4d${Math.round(screenWidth / 4)}`);
+const yRange = getDiceSet(`2d${Math.round(screenHeight / 2)}`);
+// const xRange = getDiceSet(`8d${Math.round(screenWidth / 8)}`);
+// const yRange = getDiceSet(`4d${Math.round(screenHeight / 4)}`);
 
 const body = document.body;
 
-const STAR_LIFE = 2000;
+const SPEED = 3000;
 const EASEING = 'cubic-bezier(0.55, 0.06, 0.68, 0.19)';
-const STAR_SIZE = 2;
-const STAR_SCALE = 5;
-const starsAtMoment = 600;
+const STAR_SIZE = 1;
+const STAR_SCALE = 6;
+const STARS_PER_FRAME = 3;
 
-function drawStar(id) {
+const style = document.createElement('style');
+style.appendChild(
+  document.createTextNode(`
+.star {
+  width: ${STAR_SIZE}px;
+  height: ${STAR_SIZE}px;
+  transition: transform ${SPEED}ms ${EASEING};
+}
+`)
+);
+document.head.appendChild(style);
+
+function createStar() {
   const x = xRange.roll();
   const y = yRange.roll();
-  body.appendChild(
-    create('div.star', {
-      style: {
-        width: `${STAR_SIZE}px`,
-        height: `${STAR_SIZE}px`,
-        left: `${x}px`,
-        top: `${y}px`,
-        transition: `transform ${STAR_LIFE}ms ${EASEING}`,
-      },
-      ref: bindReference(refs, `star-${id}`),
-    })
-  );
-  const posX = x - screenCenterX;
-  const posY = y - screenCenterY;
-  const hypot = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2));
-  const sin = posX / hypot;
-  const cos = posY / hypot;
-  const nextX = posX + screenCenterX * sin * 1.1;
-  const nextY = posY + screenCenterX * cos * 1.1;
-
-  let starShootTimerId = setTimeout(() => {
-    refs[`star-${id}`].style.transform = `translate(${nextX}px, ${nextY}px) scale(${STAR_SCALE})`;
-    clearTimeout(starShootTimerId);
-  }, fpsDelay);
-
-  let starDeathTimerId = setTimeout(() => {
-    refs[`star-${id}`].remove();
-    clearTimeout(starDeathTimerId);
-  }, STAR_LIFE);
+  const element = create('div.star', {
+    style: {
+      left: `${x}px`,
+      top: `${y}px`,
+    },
+  });
+  return { element, x, y };
 }
 
-let starCount = 0;
+function getNext({ x, y }) {
+  const posX = x - screenCenterX;
+  const posY = y - screenCenterY;
+  const rad = Math.atan2(posY, posX);
+  const nextX = screenCenterX * Math.cos(rad) * 1.1;
+  const nextY = screenCenterY * Math.sin(rad) * 1.1;
+  return { x: nextX, y: nextY };
+}
 
+window.t = setInterval(function tick() {
+  const constellation = [];
 
-const delay = STAR_LIFE / starsAtMoment;
+  for (let i = 0; i < STARS_PER_FRAME; i++) {
+    const star = createStar();
+    constellation.push(star);
+    body.appendChild(star.element);
+  }
 
-let drawStartTimerId = setTimeout(function tick() {
-  drawStar(starCount++);
+  setTimeout(() => {
+    constellation.forEach(star => {
+      const { x, y } = getNext(star);
+      star.element.style.transform = `translate(${x}px, ${y}px) scale(${STAR_SCALE})`;
+    })
+  }, 16);
 
-  drawStartTimerId = setTimeout(tick, 16);
+  setTimeout(() => {
+    constellation.forEach(star => {
+      star.element.remove();
+    })
+  }, SPEED);
 }, 16);
+
+window.stop = function() {
+  clearInterval(window.t);
+};
