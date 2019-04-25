26f16092d35c0484a9b9fa649bf5145c698033dd
dubaua <dubaua@gmail.com>
2019-04-25 17:17:22 +0500

    work at starfield

--- starField.js
+++ starField.js
@@ -3,34 +3,22 @@ import { getDiceSet } from './index.js';
 const field = document.getElementById('space');
 const ctx = field.getContext('2d');
 const stars = {};
-const speed = 2;
+
+// settings
+const speed = 1;
 const colorRange = getDiceSet(`d64+191`);
+const starSize = 1;
+const depth = 1;
+const entropy = 0.2;
 
 let starIndex = 0;
 let numStars = 0;
-let starSize = 1;
 let starsToDraw;
 let screenWidth;
 let screenHeight;
 let screenCenterY;
 let xRange;
 let yRange;
-let depth = true;
-let entropy = 0.2;
-
-function onResize() {
-  screenWidth = window.innerWidth;
-  screenHeight = window.innerHeight;
-  screenCenterY = Math.round(screenHeight / 2);
-  xRange = getDiceSet(`d${Math.round(screenWidth)}`);
-  yRange = getDiceSet(`2d${Math.round(screenHeight / 2)}`);
-  field.width = screenWidth;
-  field.height = screenHeight;
-  starsToDraw = (field.width * field.height) / 1e3;
-}
-
-onResize();
-window.addEventListener('resize', onResize);
 
 class Star {
   constructor(initial) {
@@ -39,7 +27,7 @@ class Star {
 
     this.distance = Math.abs(this.y - screenCenterY) / screenCenterY;
     this.acceleration = speed;
-    this.acceleration += depth ? easingSinusoidalIn(this.distance) : 0;
+    this.acceleration += depth * easingSinusoidalIn(this.distance);
     this.acceleration += entropy - Math.random() * entropy;
 
     starIndex++;
@@ -64,6 +52,15 @@ class Star {
 
 function draw(initial) {
   return function() {
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    screenCenterY = Math.round(screenHeight / 2);
+    xRange = getDiceSet(`d${Math.round(screenWidth)}`);
+    yRange = getDiceSet(`2d${Math.round(screenHeight / 2)}`);
+    field.width = screenWidth;
+    field.height = screenHeight;
+    starsToDraw = (screenWidth * screenHeight) / 1e3;
+
     ctx.fillStyle = `rgba(0, 0, 0, 1)`;
     ctx.fillRect(0, 0, field.width, field.height);
   
